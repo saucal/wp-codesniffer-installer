2c5e46a415d6e3b7421d8dfc958def968f1b0c06
Matias Saggiorato <matiassaggiorato@gmail.com>
2018-11-21 20:59:00 -0300

    Improve composer install, and add phpcs installer

--- install-standards.sh
+++ install-standards.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
 
-EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+### Install Composer
+EXPECTED_SIGNATURE="$(php -r "echo file_get_contents('https://composer.github.io/installer.sig');")"
+php -r "copy('https://getcomposer.org/installer', '$EXPECTED_SIGNATURE.php');"
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', '$EXPECTED_SIGNATURE.php');")"
 
 if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
 then
 	>&2 echo 'ERROR: Invalid installer signature'
-	rm composer-setup.php
+	php -r "unlink('$EXPECTED_SIGNATURE.php');"
 	exit 1
 fi
 
-php composer-setup.php --quiet
-RESULT=$?
-rm composer-setup.php
-echo $RESULT
+php $EXPECTED_SIGNATURE.php
+php -r "unlink('$EXPECTED_SIGNATURE.php');"
 
 ### PHPCS
-composer global require "squizlabs/php_codesniffer=*"
-
-# Add to your .bash_profile
-export PATH=$PATH:~/.composer/vendor/bin
-ln -s ~/.composer/vendor/squizlabs/php_codesniffer/bin/phpcs /usr/local/bin/phpcs
-ln -s ~/.composer/vendor/squizlabs/php_codesniffer/bin/phpcbf /usr/local/bin/phpcbf
+composer global require "dealerdirect/phpcodesniffer-composer-installer"
+rm /usr/local/bin/phpcs
+ln -s $HOME/.composer/vendor/squizlabs/php_codesniffer/bin/phpcs /usr/local/bin/phpcs
+rm /usr/local/bin/phpcbf
+ln -s $HOME/.composer/vendor/squizlabs/php_codesniffer/bin/phpcbf /usr/local/bin/phpcbf
 
 #### WordPress Coding Standards
 composer global require "wp-coding-standards/wpcs"
