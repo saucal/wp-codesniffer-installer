36a3ea9fc29960230fa792c616a7aa50dd9167f8
Matias Saggiorato <matiassaggiorato@gmail.com>
2024-09-26 14:10:07 -0300

    Remove the need for root

--- install-standards.sh
+++ install-standards.sh
@@ -23,11 +23,6 @@ fi
 ### PHPCS
 composer global config allow-plugins.dealerdirect/phpcodesniffer-composer-installer true
 composer global require -W "dealerdirect/phpcodesniffer-composer-installer" "phpcompatibility/php-compatibility:dev-develop" "wp-coding-standards/wpcs" "automattic/vipwpcs" "automattic/phpcs-neutron-ruleset"
-GLOBAL_COMPOSER_PATH=$(composer global config home --quiet)
-rm -f /usr/local/bin/phpcs
-rm -f /usr/local/bin/phpcbf
-ln -s "${GLOBAL_COMPOSER_PATH}/vendor/squizlabs/php_codesniffer/bin/phpcs" /usr/local/bin/phpcs
-ln -s "${GLOBAL_COMPOSER_PATH}/vendor/squizlabs/php_codesniffer/bin/phpcbf" /usr/local/bin/phpcbf
 
 phpcs --config-set default_standard WordPress-Extra
 phpcs --config-set show_warnings 1
