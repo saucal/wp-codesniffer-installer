52373b3dce8da362eeceb3696bbed404658796dc
Matias Saggiorato <matiassaggiorato@gmail.com>
2024-09-26 13:49:29 -0300

    Add PHP Neutron standard

--- install-standards.sh
+++ install-standards.sh
@@ -19,7 +19,7 @@ php "$EXPECTED_SIGNATURE.php" --install-dir=/usr/local/bin --filename=composer
 php -r "unlink('$EXPECTED_SIGNATURE.php');"
 
 ### PHPCS
-composer global require -W "dealerdirect/phpcodesniffer-composer-installer:0.7.0" "phpcompatibility/php-compatibility" "wp-coding-standards/wpcs" "automattic/vipwpcs" "woocommerce/woocommerce-sniffs"
+composer global require -W "dealerdirect/phpcodesniffer-composer-installer:0.7.0" "phpcompatibility/php-compatibility" "wp-coding-standards/wpcs" "automattic/vipwpcs" "woocommerce/woocommerce-sniffs" "automattic/phpcs-neutron-ruleset"
 GLOBAL_COMPOSER_PATH=$(composer global config home --quiet)
 rm /usr/local/bin/phpcs
 rm /usr/local/bin/phpcbf
